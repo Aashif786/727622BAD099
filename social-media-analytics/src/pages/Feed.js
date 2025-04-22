fbea1b147c78064061ac696e005e0de17fcc6e9a
Aashif786 <aashifanshaf786@gmail.com>
2025-04-22 12:36:03 +0530

    ..

--- social-media-analytics/src/pages/Feed.js
+++ social-media-analytics/src/pages/Feed.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Feed = () => {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch('http://20.244.56.144/evaluation-service/users/:userid/posts');
+            const data = await response.json();
+            const sortedPosts = data.sort((a, b) => b.id - a.id);
+            setPosts(sortedPosts);
+        };
+        fetchPosts();
+
+        const interval = setInterval(fetchPosts, 5000);
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div className="container mt-4 mrg">
             <h1 className='text-center'>Feed</h1>
